b04e6d43b3a34a24ca9d79aa882589992a1e1296
AndrewDushenin <dushenin.andrew@gmail.com>
2022-02-21 13:24:40 +0200

    first commit

---
+++ learn.js
@@ -0,0 +1,60 @@
+
+// console.log(jopa(5,3));
+// function jopa(a,b) {
+//     return a+b;
+// }
+
+
+// console.log(jopa1(5,4));
+// let jopa1 = function(a,b) {
+//     return a+b;
+// }
+
+// let jopa2 = (a,b) => a+b;
+// console.log(jopa2(5,4))
+
+// const obj = {
+//     name: 'Andrew',
+//     age: 25
+// }
+
+// let a = obj.name;
+
+// obj.name = 'Petia';
+
+// a = {...obj};
+// a = Object.assign(obj);
+
+
+// a.name = 'Maksim';
+
+// console.log(obj);
+// console.log(a);
+
+// function Animal() = {
+//     this.name = 'Andrew'
+// }
+
+// let dog = new Animal();
+
+// this.name;
+
+
+// function Animal(name) {
+//     constructor = Animal;
+//     this.name = name;
+// }
+
+// class Animal() {
+//     constructor
+// }
+
+// Math.random();
+// Math.float();
+// Math.PI();
+
+// a ** b;
+
+// if (!!a)
+
+//if((a<b) && (b>0 || true))
