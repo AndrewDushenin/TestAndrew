f03159ddeb318715dc794d90ea7164fe1e17aef2
AndrewDushenin <dushenin.andrew@gmail.com>
2022-02-23 17:24:59 +0200

    night

--- axios.js
+++ axios.js
@@ -1,11 +1,24 @@
+const figlet = require('figlet');
+const colors = require('colors/safe');
 const axios = require('axios');
+figlet.text('Your result : ',function (error, data) { if (error) console.error(error); else console.log(data);});
+
+console.log('--------------------------------------------------------------------------');
+
 
 async function getStrHero() {
-    try {
+    try { 
     
-    const response = await axios.get('https://api.opendota.com/api/heroes')
-        .then((response)=> response.data.filter(item => item['primary_attr'] == 'str'));
-        console.log(response);
+    const response = await axios.get('https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=20')
+        
+        const resultResponce = response.data;
+
+        [...resultResponce.asks].slice(0,10).reverse().forEach(item => console.log(colors.red(item[0],item[1])));
+        //resultResponce.asks.slice(0,10).map(item => console.log(colors.red(item[0],item[1]))).reverse();
+
+        resultResponce.bids.slice(0,10).forEach(item => console.log(colors.green(item[0],item[1])));
+
+        console.log('--------------------------------------------------------------------------');
 
     } catch (error) {
       console.error(error);
